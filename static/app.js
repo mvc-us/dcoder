058f0efc353345438c5546fdf4f86d2dfad7c78d
Michael Chen <mc.michaelchen.us@gmail.com>
2016-03-03 14:58:48 -0800

    Spinner on splash

--- static/app.js
+++ static/app.js
@@ -371,7 +371,7 @@ function init() {
           var loading_screen = pleaseWait({
             logo: "/static/dCoder_small.png",
             backgroundColor: '#E8E9EC',
-            loadingHtml: "Reading your document..."
+            loadingHtml: '<p>Reading your document...</p><div class="sk-cube-grid"><div class="sk-cube sk-cube1"></div><div class="sk-cube sk-cube2"></div><div class="sk-cube sk-cube3"></div><div class="sk-cube sk-cube4"></div><div class="sk-cube sk-cube5"></div><div class="sk-cube sk-cube6"></div><div class="sk-cube sk-cube7"></div><div class="sk-cube sk-cube8"></div><div class="sk-cube sk-cube9"></div></div>'
           });
           setTimeout(function() {
             loading_screen.finish();
